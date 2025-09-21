9a6364eedf92e9e895326e6ef71f531ffadcfa6a
Kevin Cooke <kevin@searchandbefound.com>
2025-09-20 23:05:00 -0400

    boom

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   output: 'export',
   reactStrictMode: true,
   experimental: {
+    cache: true,
     serverActions: { allowedOrigins: ['localhost:3000', 'charlottecarshows.com'] },
   },
   images: {
