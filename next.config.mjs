232599b0516bf2e192455a3d455942c46740bb09
Kevin Cooke <kevin@searchandbefound.com>
2025-09-21 22:16:41 -0400

    Add / through true and middleware.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  // Ensure pages are exported as directories (e.g. /events/index.html -> /events/)
+  trailingSlash: true,
   reactStrictMode: true,
   experimental: {
     serverActions: { allowedOrigins: ['localhost:3000', 'charlottecarshows.com'] },
