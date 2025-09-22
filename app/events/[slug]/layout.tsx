3a1aad951bf52b9f0ad1e2f53178a34915b7aa10
Kevin Cooke <kevin@searchandbefound.com>
2025-09-22 00:42:44 -0400

    removed layout.

--- app/events/[slug]/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactNode } from "react";
-
-export default function EventDetailLayout({ children }: { children: ReactNode }) {
-  return (
-    <div>
-      <div className="container py-6">{children}</div>
-    </div>
-  );
-}
